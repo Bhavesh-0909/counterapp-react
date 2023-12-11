87782c8938ba11a014ca2f73f668f50d70556efe
[Bhavesh-0909] <133791708+Bhavesh-0909@users.noreply.github.com>
2023-12-11 20:17:51 +0530

    Created a counter app just to practise usestate and done some handson react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  let [value, setValue] = useState(0);
+  function increment(){
+    value += 1;
+    console.log(value);
+    setValue(value);
+  }
+  function decrement(){
+    value -= 1;
+    console.log(value);
+    setValue(value);
+  }
+  return(
+    <>
+    <div>
+      <h1 className='h1'>Counter App</h1>
+      <div className="App">
+        <div onClick={increment}>+</div>
+        <div>{value}</div>
+        <div onClick={decrement}>-</div>        
+      </div>
     </div>
-  );
+    
+  </>
+  )
 }
+ 
 
 export default App;
